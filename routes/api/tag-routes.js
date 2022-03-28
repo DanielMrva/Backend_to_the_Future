be48a35cbf8024e630226f2a9a4d7815bd0fa588
Daniel Mrva <mrva.paints@gmail.com>
2022-03-27 21:59:12 -0600

    categories routes do not have associated products listed, tag routes by ID are not working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,9 +6,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [
-        {model: Product}
-      ],
+      include: [{model: Product}]
     });
     res.status(200).json(tagData)
   } catch (err) {
@@ -21,9 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [
-        {model: Product}
-      ],
+      include: [{model: Product}]
     });
     if (!tagData) {
       res.status(404).json({message: 'No tags found by that ID'});
