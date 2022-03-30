6942777e5007dd7a5804bb460b7af8f3e51faff2
Daniel Mrva <mrva.paints@gmail.com>
2022-03-30 17:06:58 -0600

    fixed minor issue with tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,12 +57,12 @@ router.put('/:id', async (req, res) => {
           id: req.params.id
         }
     });
-
+    
     if (!tagData) {
       res.status(404).json({message: 'No tag found by that ID'});
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err)
   }
